0a75cbc78bac32b2c8d298de29241e08f0e249a3
natanaelvich <taelima1997@gmail.com>
2023-06-25 22:59:52 -0300

    remove component unused

--- src/componentes/Player.tsx
+++
@@ -1,7 +0,0 @@
-export const Player = () => {
-  return (
-    <div className="h-full w-full flex justify-center items-center">
-      <div className="text-2xl">Hello world!</div>
-    </div>
-  );
-};
