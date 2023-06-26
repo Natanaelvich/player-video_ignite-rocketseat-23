205cc7721ada3f540ef91c04ba6ff917720bcd33
natanaelvich <taelima1997@gmail.com>
2023-06-25 23:05:36 -0300

    fix eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { Player } from "./pages/Player";
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { Player } from './pages/Player'
 
 function App() {
   return (
     <Provider store={store}>
       <Player />
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
