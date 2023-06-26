cd3c2c5b414a3af86729736377ed4e466308f26d
natanaelvich <taelima1997@gmail.com>
2023-06-26 20:07:01 -0300

    add vide component

---
+++ src/components/Video.tsx
@@ -0,0 +1,14 @@
+import ReactPlayer from 'react-player'
+
+export function Video() {
+  return (
+    <div className="aspect-video w-full bg-zinc-950">
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        controls
+        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+      />
+    </div>
+  )
+}
