7929cd8632bbfa9903c92cf5c3894008f63daa4b
natanaelvich <taelima1997@gmail.com>
2023-06-26 20:12:17 -0300

    update with state redux

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,8 +3,11 @@ import { MessageCircle } from 'lucide-react'
 import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
+import { useAppSelector } from '../store'
 
 export function Player() {
+  const modules = useAppSelector((state) => state.player.course.modules)
+
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -23,21 +26,16 @@ export function Player() {
           </div>
 
           <aside className="absolute bottom-0 right-0 top-0 w-80 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module
-              moduleIndex={0}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={1}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={2}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
+            {modules.map((module, index) => {
+              return (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLessons={module.lessons.length}
+                />
+              )
+            })}
           </aside>
         </main>
       </div>
