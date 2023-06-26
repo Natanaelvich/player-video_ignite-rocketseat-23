1bb73c37296e94a75cf2f4ae2339de0b889c3734
natanaelvich <taelima1997@gmail.com>
2023-06-26 20:06:55 -0300

    add module component

---
+++ src/components/Module.tsx
@@ -0,0 +1,37 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
+
+import { ChevronDown } from 'lucide-react'
+import { Lesson } from './Lesson'
+
+interface ModuleProps {
+  moduleIndex: number
+  title: string
+  amountOfLessons: number
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  return (
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
+          {moduleIndex + 1}
+        </div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+        </div>
+
+        <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do redux" duration="09:13" />
+          <Lesson title="Fundamentos do redux" duration="09:13" />
+          <Lesson title="Fundamentos do redux" duration="09:13" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+}
