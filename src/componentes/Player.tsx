84a9c75bd5edcc445a32186b3585b530f293a038
natanaelvich <taelima1997@gmail.com>
2023-06-25 22:54:10 -0300

    add initial componente player

---
+++ src/componentes/Player.tsx
@@ -0,0 +1,7 @@
+export const Player = () => {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="text-2xl">Hello world!</div>
+    </div>
+  );
+};
