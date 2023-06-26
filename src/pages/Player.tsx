0c362c4eaedec49a39737bbdf66b596e43359fcc
natanaelvich <taelima1997@gmail.com>
2023-06-25 23:08:39 -0300

    fix eslint error react/no-unescaped-entities

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -9,7 +9,7 @@ export function Player() {
           <div className="flex flex-col gap-1">
             <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
             <span className="text-sm text-zinc-400">
-              Módulo "Desvendando o Redux"
+              Módulo &quot;Desvendando o Redux&quot;
             </span>
           </div>
 
