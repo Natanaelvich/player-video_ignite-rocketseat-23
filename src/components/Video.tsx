78f84209b920052c7d0c2f869fa53eaed01f53af
natanaelvich <taelima1997@gmail.com>
2023-06-27 16:15:35 -0300

    add hook current lesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,11 @@
 import ReactPlayer from 'react-player'
-import { useAppDispatch, useAppSelector } from '../store'
-import { next } from '../store/slices/player'
+import { useAppDispatch } from '../store'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
   const dispatch = useAppDispatch()
 
-  const lesson = useAppSelector((state) => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
     dispatch(next())
@@ -26,7 +17,7 @@ export function Video() {
         width="100%"
         height="100%"
         controls
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
         playing
         onEnded={handlePlayNext}
       />
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,9 +4,17 @@ import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
 import { useAppSelector } from '../store'
+import { useEffect } from 'react'
+import { useCurrentLesson } from '../store/slices/player'
 
 export function Player() {
   const modules = useAppSelector((state) => state.player.course.modules)
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`
+  }, [currentLesson])
 
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
