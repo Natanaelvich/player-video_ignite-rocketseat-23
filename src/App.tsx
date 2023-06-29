30d4e7aff9a0e98b6475085d38238dcdc9fae603
natanaelvich <taelima1997@gmail.com>
2023-06-28 22:28:50 -0300

    remove provider redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Provider } from 'react-redux'
-import { store } from './store'
 import { Player } from './pages/Player'
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Player />
-    </Provider>
-  )
+  return <Player />
 }
 
 export default App
